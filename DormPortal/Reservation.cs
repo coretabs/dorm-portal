276a69ec32e154469101f717ca6b59532917060c
0xRumple <kasseryasser@windowslive.com>
2018-04-09 00:05:13 +0300

    Add simple models

---
+++ DormPortal/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormPortal
+{
+    public class Reservation
+    {
+	    public Student Student { get; set; }
+	    public Room Room { get; set; }
+    }
+}
---
+++ DormPortal/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DormPortal
+{
+    public class Student
+    {
+	    public string Name { get; set; }
+	    public string PassportNumber { get; set; }
+    }
+}
