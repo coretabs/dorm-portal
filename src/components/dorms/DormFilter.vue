0557d18e38ace4e6346b4e771353f35ebb53ac41
Alhakeem <alhakeem.prof@gmail.com>
2018-11-05 16:18:37 +0300

    Update filter layout

--- src/components/dorms/DormFilter.vue
+++ src/components/dorms/DormFilter.vue
@@ -4,32 +4,22 @@
         <v-navigation-drawer
       :clipped="$vuetify.breakpoint.lgAndUp"
       v-model="drawer"
-      app
       >
 
         <template>
           <div id="filters-heading"><v-icon left>filter_list</v-icon> {{filtersHeading}}</div>
 
-          <div id="filters-body">
+         <div id="filters-body">
             <div class="filter" v-for= "(filter,index) in filters" :key="index">
               <div class="filter-title">{{filter.title}}</div>
               <ul>
                 <li v-for= "(option, index) in filter.options" :key="index">
-                <v-checkbox v-model="checkboxs" :value="option" :label="option"></v-checkbox>
+                  <v-checkbox v-model="checkboxs" :value="option" :label="option" color="success"></v-checkbox>
                 </li>
               </ul>
             </div>
-          </div>
+          </div> 
 
-
-          <!-- <v-list-tile v-for= "(filter,index) in filters" :key="index" @click="">
-            <v-list-tile-action>
-              <v-checkbox v-model="checkboxs" :value="option"></v-checkbox>
-            </v-list-tile-action>
-            <v-list-tile-content @click="checkboxs = !checkboxs">
-              <v-list-tile-title>{{filter.title}}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile> -->
 
         </template>
 
@@ -84,7 +74,7 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import '../../assets/styles/vars';
 @import '../../assets/styles/mixins';
 #filters{
@@ -92,15 +82,13 @@
     box-sizing: border-box;
     outline: none;
   }
-  width: $filtersAside-width;
   height: auto;
   padding-bottom: 10px;
   background-color: #fcfcfc;
-  border-right: 1px solid rgba(0,0,0,0.1);
   position: relative;
   overflow: hidden;
   #filters-heading{
-    line-height: 45px;
+    line-height: 50px;
     padding: 2px 15px 0;
     background: #ff884e;
     background: -webkit-linear-gradient(to right, #f9ba23, #ff884e); 
@@ -108,9 +96,7 @@
     border-right: 1px solid rgba(0,0,0,.1);
     font-size: 1.3em;
     color: #000;
-    z-index: 1;
-    position: fixed;
-    width: 100%;
+    margin-bottom: 15px;
       .v-icon{
         color: #b75f11;
         margin-right: 6px;
@@ -131,7 +117,7 @@
         list-style: none;
         padding: 0;
           li{
-            padding: 0px 0px 5px 30px;
+            padding: 0px 0px 2px 30px;
             cursor: pointer;
             transition:all 0.2s;            
             .v-input,
@@ -142,14 +128,8 @@
             .v-messages{
               display: none;
             }
-            .v-input--is-label-active .theme--light.v-icon{
-              color: #68cf91 !important;
-            }
-            .theme--light.v-icon{
-              color: #aaa !important;
-            }
             .v-label{
-              padding: 6px 20px 4px 0;
+              line-height: 35px;
             }
             .v-input__control,
             .v-input__slot,
@@ -159,6 +139,9 @@
             }
             &:hover{
               background: #ccfbde;
+            }
+            .v-input--selection-controls{
+              padding: 0 !important;
             }
           }
       }
