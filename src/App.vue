1962acd941f3d168113522bedfb613dd409b1cf0
Alhakeem <alhakeem.prof@gmail.com>
2018-11-05 12:48:34 +0300

    Add DormFilter component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
     <app-header></app-header>
+    <dorm-filter></dorm-filter>
   </v-app>
 </template>
 
 <script>
   import HeaderComponent from './components/shared/Header'
+  import DormFilter from './components/dorms/DormFilter'
   export default {
     data: function (){
       return{}
     },
     components: {
-      'app-header': HeaderComponent
+      'app-header': HeaderComponent,
+      'dorm-filter': DormFilter
     }
   }
 </script>
---
+++ src/components/dorms/DormFilter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DormFilter',
+    data: function(){
+      return{
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
