3b54fe7d3e4146d988e4435ae9f663c98d1b68eb
Alhakeem <alhakeem.prof@gmail.com>
2018-11-06 21:59:40 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import '@/plugins/vuetify'
 import App from '@/App.vue'
 import router from '@/router'
-import store from "@/store";
+import store from "@/store"
 import $backend from '@/backend'
 Vue.prototype.$backend = $backend
 Vue.config.productionTip = false
@@ -10,6 +10,7 @@ Vue.config.productionTip = false
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' 
 Vue.use(Vuetify)
+
 
 // Vue.use(VueRouter)
 
