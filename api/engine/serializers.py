0c7d340b4c9b0434171e3572d5d74fa23f628f7b
0xRumple <kasseryasser@windowslive.com>
2018-11-11 15:02:54 +0200

    Cleanup template non used code

---
+++ api/engine/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+#class MessageSerializer(serializers.HyperlinkedModelSerializer):
+#    class Meta:
+#        model = Message
+#        fields = ('url', 'subject', 'body', 'pk')
