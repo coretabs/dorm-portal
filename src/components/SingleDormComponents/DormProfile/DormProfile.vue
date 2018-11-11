f5be8354e773607d6c924fd3061fa108141e7131
Alhakeem <alhakeem.prof@gmail.com>
2018-11-11 21:26:45 +0300

    Add Dorm cover section

--- src/components/SingleDormComponents/DormProfile/DormProfile.vue
+++ src/components/SingleDormComponents/DormProfile/DormProfile.vue
@@ -1,16 +1,28 @@
 <template>
 <div id="dorm-profile">
   <v-content>
-    <v-container fluid>
-      <v-layout row wrap>
-        
-        asdasd
-        
+
+    <v-parallax dark src="https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+      <v-layout justify-center>
+
+        <v-flex md8 xs10>
+          <section id="dorm-banner">
+            <h1>Alfam Dormitory</h1>
+            <div>
+              <v-icon>place</v-icon>
+              <span>Kaleland street bla bla</span> - 
+              <a href="#" @click.stop.prevent="showMap"> show map</a>
+            </div>
+          </section>
+        </v-flex>
+
       </v-layout>
-    </v-container>
+    </v-parallax>
+
   </v-content>
 </div>
 </template>
 
 <script src="./DormProfile.js"></script>
+
 <style src="./DormProfile.scss" lang="scss"></style>
