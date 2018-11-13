6b3bf258a7180497937857df54e80a64a6dbbe7e
Alhakeem <alhakeem.prof@gmail.com>
2018-11-13 16:00:10 +0300

    Update room card layout

--- src/components/SingleDormComponents/RoomCard/RoomCard.js
+++ src/components/SingleDormComponents/RoomCard/RoomCard.js
@@ -10,7 +10,10 @@ export default {
   data: function () {
     return {
       swiperOption: {
-        slidesPerView: 1,
+        grabCursor: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 10,
         pagination: {
           el: '.swiper-pagination',
           clickable: true
@@ -19,7 +22,41 @@ export default {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         }
-      }
+      },
+      features:  [
+        {
+          name: "free Wifi",
+          icon: "fa-wifi"
+      },
+      {
+          name: "Restaurant",
+          icon: "fa-utensils"
+      },
+      {
+          name: "Laundry",
+          icon: "local_laundry_service"
+      },
+      {
+        name: "Market",
+        icon: "fa-shopping-cart"
+      },
+      {
+        name: "Elevator",
+        icon: "fa-check"
+      },
+      {
+        name: "Air condition",
+        icon: "fa-wind"
+      },
+      {
+        name: "Fire alarm",
+        icon: "fa-fire"
+      },
+      {
+        name: "Barber",
+        icon: "fa-cut"
+      }       
+    ]
     };
   },
   methods: {
@@ -29,5 +66,7 @@ export default {
     lang() {
       return this.$store.getters.lang;
     }
+  },
+  created(){
   }
 };
--- src/components/SingleDormComponents/RoomCard/RoomCard.vue
+++ src/components/SingleDormComponents/RoomCard/RoomCard.vue
@@ -4,62 +4,71 @@
     <v-layout row wrap>
 
       <v-flex class="room-img" xs12 sm8>
-        <v-flex class="room-img" xs12 sm12>
 
-          <swiper :options="swiperOption">
-            <swiper-slide>
-              <v-img src="https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)" height="100%" width="100%"></v-img>
-            </swiper-slide>
-            <swiper-slide>
-              <v-img src="https://images.pexels.com/photos/1326946/pexels-photo-1326946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)" height="100%" width="100%"></v-img>
-            </swiper-slide>
-            <swiper-slide>
-              <v-img src="https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)" height="100%" width="100%"></v-img>
-            </swiper-slide>
-            <div class="swiper-pagination" slot="pagination"></div>
-            <div class="swiper-button-prev" slot="button-prev"></div>
-            <div class="swiper-button-next" slot="button-next"></div>
-          </swiper>
+        <swiper :options="swiperOption">
+          <swiper-slide>
+            <v-img position src="https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)"></v-img>
+          </swiper-slide>
+          <swiper-slide>
+            <v-img position src="https://images.pexels.com/photos/1326946/pexels-photo-1326946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)"></v-img>
+          </swiper-slide>
+          <swiper-slide>
+            <v-img position src="https://images.pexels.com/photos/279719/pexels-photo-279719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" gradient="to top right, rgba(44,40,72,.4), rgba(44,40,72,.4)"></v-img>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
 
-        </v-flex>
+      </v-flex>
 
-        <v-flex class="room-details" xs12 sm12>
+      <v-flex class="room-details" xs12 sm4>
 
-          <v-layout row wrap>
+        <v-layout row wrap justify-center align-center>
 
-            <v-flex sm3>
-              <h3>Room Type:</h3>
-              <span>Suit Room</span>
-            </v-flex>
+          <v-flex class="detail-block" xs6 sm6>
+            <h3>Room Type:</h3>
+            <span>Suit Room</span>
+          </v-flex>
 
-            <v-flex sm3>
-              <h3>Number of People:</h3>
-              <span>
+          <v-flex class="detail-block" xs6 sm6>
+            <h3>Number of People:</h3>
+            <span>
                 <v-icon>fa-user</v-icon>
                 X 2
               </span>
-            </v-flex>
+          </v-flex>
 
-            <v-flex sm3>
-              <h3>Price:</h3>
-              <span>$1555</span>
-            </v-flex>
+         
 
-            <v-flex sm3>
-              <v-btn color="success" class="elevation-0">Reserve Now</v-btn>
-            </v-flex>
-
-          </v-layout>
-
-          <template>
-            <v-layout class="room-warning">
+          <v-flex class="detail-block" xs12>
+            <div class="room-warning">
               <v-icon>warning</v-icon>
               <span>5 {{lang.dormCard.roomsLeft}}</span>
-            </v-layout>
-          </template>
-          <v-layout row wrap class="room-facilities">
+            </div>
+          </v-flex>
 
-            <!-- <v-flex xs12>
+          <v-flex class="feature-block" xs12>
+            <h3>Room Features:</h3>
+            <div class="room-feature" v-for="(feature,index) in features" :key="index">
+              <v-icon>{{feature.icon}}</v-icon>
+              <span>{{feature.name}}</span>
+            </div>
+          </v-flex>
+
+          <v-flex class="room-price" xs6 sm6>
+            <h3>Price:</h3>
+            <span>$1555</span>
+          </v-flex>
+
+          <v-flex class="reserve-btn" sm6>
+            <v-btn color="success" class="elevation-0">Reserve Now</v-btn>
+          </v-flex> 
+
+        </v-layout>
+
+        <!-- <v-layout row wrap class="room-facilities">
+          <v-flex xs12>
               <h3>Room Features:</h3>
               <v-tooltip top v-for="n in 14" :key="n">
                 <v-icon class="facility-icon" slot="activator">fa-wifi</v-icon>
@@ -69,16 +78,14 @@
                 <v-icon class="facility-icon" slot="activator">more_horiz</v-icon>
                 <span>{{lang.dormCard.more}}</span>
               </v-tooltip>
-            </v-flex> -->
+            </v-flex>
+        </v-layout> -->
 
-          </v-layout>
-
-        </v-flex>
       </v-flex>
     </v-layout>
 
   </v-card>
-
+  
 </v-flex>
 </template>
 
