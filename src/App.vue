8224e3206b12e862b85b908d8e7adba4e0c53dd7
Alhakeem <alhakeem.prof@gmail.com>
2018-11-18 22:58:21 +0300

    Add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <v-app>
     <router-view></router-view>
     <app-header></app-header>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
   import HeaderComponent from './components/SharedComponents/Header/Header.vue'
   import DormFilter from './components/DormsComponents/DormFilter/DormFilter.vue'
+  import FooterComponent from './components/SharedComponents/Footer/Footer.vue'
   export default {
     data: function (){
       return{}
     },
     components: {
       'app-header': HeaderComponent,
-      'dorm-filter': DormFilter
+      'dorm-filter': DormFilter,
+      'app-footer': FooterComponent
     }
   }
 </script>
---
+++ src/components/SharedComponents/Footer/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+<v-footer class="pa-3">
+  <v-spacer></v-spacer>
+  <div>&copy; copyrights</div>
+</v-footer>
+</template>
+
+<script src="./Footer.js"></script>
+
+<style src="./Footer.scss" lang="scss"></style>
