f27b9dd32f4a4b6269f8fbfe7b2b9adcd82107a1
Alhakeem <alhakeem.prof@gmail.com>
2018-12-05 15:06:45 +0300

    Update login layout

--- src/components/ReservationComponents/Login/Login.vue
+++ src/components/ReservationComponents/Login/Login.vue
@@ -1,7 +1,7 @@
 <template>
 <v-content id="login">
   <v-container fluid fill-height>
-    <v-layout align-center justify-center>
+    <v-layout mt-5 justify-center>
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-3">
           <v-toolbar color="#fff" class="elevation-0">
