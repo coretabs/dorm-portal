730286332328ddbe830725fefdf10a5533e2e7d3
Alhakeem <alhakeem.prof@gmail.com>
2018-12-06 23:17:52 +0300

    Update rooms card component

--- src/components/ManagerComponents/ManageRooms/ManageRooms.js
+++ src/components/ManagerComponents/ManageRooms/ManageRooms.js
@@ -3,11 +3,214 @@ export default {
   name: "ManageRooms",
   data: function () {
     return {
-      quota: 5,
-      showQuotaUpdate: false,
+      showQuotaUpdatedialog: false,
+      showRoomDetailsdialog: false,
+      reservedRoomsNumber: null,
+      totalRoomsNumber: null,
+      allowedQuotaNumber: null,
+      availableRoomsNumber: null,
+      quotaRoomID: null,
+      roomDetails: {},
       items: [
         { title: 'Edit' },
         { title: 'Delete' }
+      ],
+      room_cards:[
+        {
+          id:1,
+          room_type: "Single room",
+          total_rooms: 100,
+          reserved_rooms: 50,
+          allowed_quota: 10,
+          price: 1500,
+          people: 2,
+          features1: [
+            {
+              name: "free wifi",
+              icon: "fa-wifi"
+            },
+            {
+              name: "Parking",
+              icon: "fa-check"
+            }
+          ],
+          features2:[
+            {
+              name: "meals",
+              choice: "One meal"
+            },
+            {
+              name: "another feature",
+              choice: "24/7 reception"
+            }
+          ],
+          photos:[
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/1082326/pexels-photo-1082326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            },
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            }
+          ]
+        },
+        {
+          id:2,
+          room_type: "Single room",
+          total_rooms: 20,
+          reserved_rooms: 3,
+          allowed_quota: 15,
+          price: 1500,
+          people: 2,
+          features1: [
+            {
+              name: "free wifi",
+              icon: "fa-wifi"
+            },
+            {
+              name: "Parking",
+              icon: "fa-check"
+            }
+          ],
+          features2:[
+            {
+              name: "meals",
+              choice: "One meal"
+            },
+            {
+              name: "another feature",
+              choice: "24/7 reception"
+            }
+          ],
+          photos:[
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/1082326/pexels-photo-1082326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            },
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            }
+          ]
+        },
+        {
+          id:3,
+          room_type: "Double room",
+          total_rooms: 60,
+          reserved_rooms: 59,
+          allowed_quota: 1,
+          price: 1500,
+          people: 2,
+          features1: [
+            {
+              name: "free wifi",
+              icon: "fa-wifi"
+            },
+            {
+              name: "Parking",
+              icon: "fa-check"
+            }
+          ],
+          features2:[
+            {
+              name: "meals",
+              choice: "One meal"
+            },
+            {
+              name: "another feature",
+              choice: "24/7 reception"
+            }
+          ],
+          photos:[
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/1082326/pexels-photo-1082326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            },
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            }
+          ]
+        },
+        {
+          id:4,
+          room_type: "Delux room",
+          total_rooms: 60,
+          reserved_rooms: 45,
+          allowed_quota: 5,
+          price: 1500,
+          people: 2,
+          features1: [
+            {
+              name: "free wifi",
+              icon: "fa-wifi"
+            },
+            {
+              name: "Parking",
+              icon: "fa-check"
+            }
+          ],
+          features2:[
+            {
+              name: "meals",
+              choice: "One meal"
+            },
+            {
+              name: "another feature",
+              choice: "24/7 reception"
+            }
+          ],
+          photos:[
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/1082326/pexels-photo-1082326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            },
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            }
+          ]
+        },
+        {
+          id:4,
+          room_type: "Delux room",
+          total_rooms: 20,
+          reserved_rooms: 20,
+          allowed_quota: 0,
+          price: 1500,
+          people: 2,
+          features1: [
+            {
+              name: "free wifi",
+              icon: "fa-wifi"
+            },
+            {
+              name: "Parking",
+              icon: "fa-check"
+            }
+          ],
+          features2:[
+            {
+              name: "meals",
+              choice: "One meal"
+            },
+            {
+              name: "another feature",
+              choice: "24/7 reception"
+            }
+          ],
+          photos:[
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/1082326/pexels-photo-1082326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            },
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            }
+          ]
+        }
       ]
     };
   },
@@ -19,6 +222,31 @@ export default {
   methods:{
     loadComponent(componentName){
       this.$root.$emit('currentTabComponent', componentName)
+    },
+    showRoomDetails(room){
+      this.roomDetails = room;
+      this.showRoomDetailsdialog = true;
+    },
+    showQuotaUpdate(reservedRooms, totalRooms, allowedQuota, roomID){
+      this.reservedRoomsNumber = reservedRooms;
+      this.totalRoomsNumber = totalRooms;
+      this.allowedQuotaNumber = allowedQuota;
+      this.quotaRoomID = roomID;
+      this.showQuotaUpdatedialog = true;
+    },
+    progressValue(reserved_rooms,total_rooms){
+      return reserved_rooms / total_rooms * 100;
+    },
+    availableRooms(reserved_rooms, total_rooms){
+      this.availableRoomsNumber = total_rooms - reserved_rooms;
+      return this.availableRoomsNumber;
+    },
+    progressColor(reservedRooms, totalRooms){
+
+      var avg = reservedRooms/totalRooms*100;
+
+      return (avg > 90) ? "#cf5151" : (avg > 50) ? "#ec7811" : "#39c463";
+      
     }
   }
 };
--- src/components/ManagerComponents/ManageRooms/ManageRooms.vue
+++ src/components/ManagerComponents/ManageRooms/ManageRooms.vue
@@ -4,7 +4,8 @@
     <v-btn color="success" dark medium fixed bottom right fab @click="loadComponent('AddNewRoom')">
       <v-icon>add</v-icon>
     </v-btn>
-    <v-flex xs12 v-for="n in 5" :key="n">
+
+    <v-flex xs12 v-for="(room,i) in room_cards" :key="i">
       <v-card id="room-card">
         <v-menu bottom left class="room-card__actions absolute-menu">
           <v-btn slot="activator" icon>
@@ -21,51 +22,66 @@
             <v-layout wrap>
               <v-flex class="room-card__title" xs12>
                 <h3>Room Type:</h3>
-                <span>Single room</span>
-                <v-btn slot="activator" icon>
-                  <v-icon>fa-info</v-icon>
+                <span>{{ room.room_type }}</span>
+                <v-btn slot="activator" @click="showRoomDetails(room)" icon>
+                  <v-icon >fa-info</v-icon>
                 </v-btn>
               </v-flex>
             </v-layout>
           </v-flex>
+          
           <v-flex class="room-card__title" xs12 md8>
             <v-layout class="room-card__quantity" wrap>
-              <v-flex xs12 sm6 md8>
+              <v-flex xs12 sm6 md10>
                 <h3>Reserved Rooms:</h3>
                 <v-layout row wrap>
                   <v-flex xs12 md2>
-                    <span>20<sub>/100</sub></span>
+                    <span>{{ room.reserved_rooms }}<sub>/{{ room.total_rooms }}</sub></span>
                   </v-flex>
                   <v-flex xs12 md10>
-                    <v-progress-linear class="room-card__progress" color="green" height="25" value="20"></v-progress-linear>
+                    <v-progress-linear class="room-card__progress" :color="progressColor(room.reserved_rooms, room.total_rooms)" height="25" :value="progressValue(room.reserved_rooms, room.total_rooms)"></v-progress-linear>
                   </v-flex>
                 </v-layout>
                 <!-- <span>0<sub>/50</sub></span> -->
               </v-flex>
 
-              <v-flex xs12 sm6 md4>
+              <v-flex xs12 sm6 md2>
                 <h3>Quota:</h3>
                 <v-layout row>
-                  <span>0</span>
-                  <v-btn icon class="quota-update" dark @click="showQuotaUpdate = true">
-                    <v-icon small>fa-pencil-alt</v-icon>
+                  <span>{{ room.allowed_quota }}</span>
+                  <v-btn icon class="quota-update" dark @click="showQuotaUpdate(room.reserved_rooms, room.total_rooms, room.allowed_quota, room.id)">
+                    <v-icon small >fa-pencil-alt</v-icon>
                   </v-btn>
                 </v-layout>
               </v-flex>
+              
             </v-layout>
           </v-flex>
         </v-card-title>
       </v-card>
     </v-flex>
-  </v-layout>
-  <v-layout row justify-center>
-    <v-dialog v-model="showQuotaUpdate" persistent max-width="350">
+
+    <v-dialog v-model="showQuotaUpdatedialog" persistent max-width="350">
       <v-card>
         <v-card-text>
           <h2>Update Quota:</h2>
-          <p class="avaliable-quota__info">You can add upto <strong>45</strong> room</p>
-          <p class="avaliable-quota__warning">You have <strong>0</strong> room to add</p>
-          <v-text-field v-model="quota" type="number" min="0" required></v-text-field>
+          <p v-if=" availableRooms(reservedRoomsNumber, totalRoomsNumber) > 0 " class="avaliable-quota__info">You can add upto <strong>{{  availableRoomsNumber }}</strong> room</p>
+          <p v-else class="avaliable-quota__warning">You have <strong>0</strong> room to add</p>
+          <v-text-field v-model="allowedQuotaNumber" type="number" min="0" :max="availableRoomsNumber" required :disabled="availableRoomsNumber == 0"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn  flat @click="showQuotaUpdatedialog = false">Cancel</v-btn>
+          <v-btn v-if="availableRoomsNumber > 0" color="green darken-1" flat @click="showQuotaUpdatedialog = false">Confirm</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="showRoomDetailsdialog"  max-width="900">
+      <v-card>
+        <v-card-text>
+          <h2>{{ roomDetails.room_type }}</h2>
+          
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -74,6 +90,11 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+
+  </v-layout>
+  <v-layout row justify-center>
+    
   </v-layout>
 </div>
 </template>
