79c0ae944be251c8a525b6c08ee79fe92c403d23
0xRumple <kasseryasser@windowslive.com>
2018-12-15 01:40:52 +0200

    Add seed command

---
+++ api/engine/management/commands/seed.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from features.steps import factory
+
+
+class Command(BaseCommand):
+    help = 'Seed the database'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Seeding the database...')
+        factory.create_alfam_dovec_with_4_rooms(self)
+        self.stdout.write('Done !')
