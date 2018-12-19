2513dc5468d38b89e7e1775f56f350822d5a20d2
0xRumple <kasseryasser@windowslive.com>
2018-12-19 18:52:48 +0200

    Simplify password validation

--- api/settings/base.py
+++ api/settings/base.py
@@ -64,16 +64,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
     },
 ]
 
