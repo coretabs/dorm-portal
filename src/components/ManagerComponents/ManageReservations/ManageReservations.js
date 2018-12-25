0854e14a14cc888e581d8796ed4f7c0b13cbb98f
Alhakeem <alhakeem.prof@gmail.com>
2018-12-25 17:18:23 +0300

    reset reservation update status data

--- src/components/ManagerComponents/ManageReservations/ManageReservations.js
+++ src/components/ManagerComponents/ManageReservations/ManageReservations.js
@@ -85,9 +85,9 @@ export default {
       this.reservationID = item.id
     },
     close(){
-      this.currentStatus = '',
-      this.followUpMessage = '',
-      this.date = '',
+      this.currentStatus = null,
+      this.followUpMessage = null,
+      this.date = null,
       this.showUpdateStatus = false
     },
     fetchManagerReservation(){
