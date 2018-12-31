18314ac1748a974fc550fb7bfbe13a06cb36fe2d
Alhakeem <alhakeem.prof@gmail.com>
2018-12-31 03:43:42 +0300

    Mask user name in reviews

--- src/components/DormsComponents/DormReviews/DormReviews.js
+++ src/components/DormsComponents/DormReviews/DormReviews.js
@@ -8,6 +8,13 @@ export default {
     return {
     }
   },
+  methods:{
+    maskName(name){
+      return name.toLowerCase().split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + Array(s.length-1).fill("*").join(''))
+      .join(' ');
+    }
+  },
   computed: {
     lang() {
       return this.$store.getters.lang;
