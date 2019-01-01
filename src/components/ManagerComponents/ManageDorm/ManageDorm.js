6f9de0f79a8c03065b2258d4eb3e7e8b1ac65cdd
Alhakeem <alhakeem.prof@gmail.com>
2019-01-01 04:40:02 +0300

    Remove unnecessary escape character

--- src/components/ManagerComponents/ManageDorm/ManageDorm.js
+++ src/components/ManagerComponents/ManageDorm/ManageDorm.js
@@ -72,7 +72,7 @@ export default {
       ],
       urlRules: [
         v => !!v || 'URL is required',
-        v => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(v) || 'URL must be valid, Ex: https://emu.edu.tr'
+        v => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/.test(v) || 'URL must be valid, Ex: https://emu.edu.tr'
       ]
     };
   },
