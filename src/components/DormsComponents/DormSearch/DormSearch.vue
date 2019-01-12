76bff69b79d595a50ee932ca9bf80043e66a5ecd
Alhakeem <alhakeem.prof@gmail.com>
2019-01-12 19:43:07 +0300

    Update search inputs prepend icons

--- src/components/DormsComponents/DormSearch/DormSearch.vue
+++ src/components/DormsComponents/DormSearch/DormSearch.vue
@@ -3,14 +3,14 @@
   <h2>{{lang.dormSearch.heading}}</h2>
   <v-layout row wrap>
     <v-flex xs12 md3>
-      <v-select :items="category" item-text="name" item-value="id" label="Dormitories type" prepend-inner-icon="meeting_room" solo color="success" append-icon="expand_more" :menu-props="{
+      <v-select :items="category" item-text="name" item-value="id" label="Dormitories type" prepend-inner-icon="fa-hotel" solo color="success" append-icon="expand_more" :menu-props="{
             offsetY: '',
             transition: 'slide-y-transition',
             bottom: ''
           }" v-model="chosenCategory"></v-select>
     </v-flex>
     <v-flex xs12 md3>
-      <v-select class="shift-left" :items="dutarion" item-text="name" item-value="id" label="Duration" prepend-inner-icon="calendar_today" solo color="success" append-icon="expand_more" :menu-props="{
+      <v-select class="shift-left" :items="dutarion" item-text="name" item-value="id" label="Duration" prepend-inner-icon="far fa-calendar-alt" solo color="success" append-icon="expand_more" :menu-props="{
             offsetY: '',
             transition: 'slide-y-transition',
             bottom: ''
